32d11a7f2f25cdfc4d01fa658e2a0218c7ef544d
WildanulHaq <wildanulhaq0@gmail.com>
2024-06-04 00:41:17 +0700

    menambahkan program

--- Program/main.cpp
+++ Program/main.cpp
@@ -4,70 +4,72 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include "suara.h"
-// #include "suara.h"
 
 volatile bool play = false;
 volatile uint8_t track = 0;
 volatile uint16_t count = 0;
 
-ISR(TIMER0_COMPA_vect){
-  if (play){
-    if (track == 0){
-      OCR1B = ~pgm_read_byte(&gempa[count]); // Play track 0
+ISR(TIMER0_COMPA_vect) {
+  if (play) {
+    if (track == 0) {
+      OCR1A = ~pgm_read_byte(&gempa[count]); // Play track 0
       count++;
-      if (count >= sizeof(gempa)){
-        play = false;
-        OCR1B = 0;
+      if (count >= sizeof(gempa)) {
+        count = 0; // Loop the track
       }
     }
-    // else if (track == 1){
-    //   OCR1B = ~pgm_read_byte(&akhir[count]); // Play track 1
-    //   count++;
-    //   if (count >= sizeof(akhir))
-    //     play = false;
-    // }
+  } else {
+    OCR1A = 0; // Ensure the output is off if not playing
   }
 }
 
-void initPWM(void);
-void TimerInit(void);
+void initPWM(void) {
+  DDRB |= _BV(DDB1); // Set PB1 (OC1A) as output for PWM
+  TCCR1A = _BV(WGM10) | _BV(COM1A1); // Fast PWM, 8-bit
+  TCCR1B = _BV(WGM12) | _BV(CS10);   // Fast PWM, no prescaler
+  OCR1A = 0; // Ensure PWM is off initially
+}
 
-int main(void){
-  DDRD |= _BV(DDD6); // Set PD6 (OC0A/OC1A) as output for PWM
+void TimerInit(void) {
+  TCCR0A = _BV(WGM01); // CTC mode
+  TCCR0B = _BV(CS01);  // Prescaler 8
+  OCR0A = 250;
+  TIMSK0 |= _BV(OCIE0A); // Enable compare match interrupt
+}
+
+int main(void) {
+  DDRD &= ~_BV(DDD2); // Set PD2 as input (push button)
+  DDRD &= ~_BV(DDD3); // Set PD3 as input (switch)
+  DDRB |= _BV(DDB1);  // Set PB1 (OC1A) as output for PWM
+
   PORTD |= _BV(PORTD2) | _BV(PORTD3); // Enable pull-up resistors on PD2 and PD3
 
   initPWM();
   TimerInit();
   sei();
 
-  while (1){
-    if (!(PIND & _BV(PIND2)) && !play){ // PD2 as track 0 selector, detect LOW state
-      _delay_ms(50); // Debounce delay
-      if (!(PIND & _BV(PIND2))){ // Confirm button press
-        play = true;
-        track = 0;
-        count = 0;
-        while (!(PIND & _BV(PIND2))); // Wait for button release
+  bool button_pressed = false; // Flag to track button press
+
+  while (1) {
+    // Check the state of the switch first
+    if (PIND & _BV(PIND3)) { // PD3 as switch, detect HIGH state
+      play = false;
+      OCR1A = 0; // Stop playing when switch is HIGH
+    } else {
+      // If switch is LOW, then check the push button
+      if (!(PIND & _BV(PIND2))) { // PD2 as track 0 selector, detect LOW state
+        _delay_ms(50); // Debounce delay
+        if (!(PIND & _BV(PIND2))) { // Confirm button press
+          if (!button_pressed) { // Only proceed if button is not already pressed
+            button_pressed = true; // Set button pressed flag
+            play = !play; // Toggle play state
+            track = 0;
+            count = 0;
+          }
+        }
+      } else {
+        button_pressed = false; // Reset button press flag when button is released
       }
     }
-    // else if (!(PIND & _BV(PIND3))){ // PD3 as track 1 selector
-    //   play = true;
-    //   track = 1;
-    //   count = 0;
-    // }
   }
-}
-
-void initPWM(){
-  DDRB |= _BV(DDB3); // Set PB3 (OC1B) as output for PWM
-  TCCR1A = _BV(WGM10) | _BV(COM1B1); // Fast PWM, 8-bit
-  TCCR1B = _BV(WGM12) | _BV(CS10);   // Fast PWM, no prescaler
-  OCR1B = 200;
-}
-
-void TimerInit(){
-  TCCR0A = _BV(WGM01); // CTC mode
-  TCCR0B = _BV(CS01);  // Prescaler 8
-  OCR0A = 250;
-  TIMSK0 |= _BV(OCIE0A); // Enable compare match interrupt
 }
